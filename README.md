cefe82183672cea6705edf922ec489ff88766f76
Hariom-99 <hariomkhalghat@gmail.com>
2025-07-13 11:09:25 +0530

    learned about the routing and editing of the data

--- README.md
+++ README.md
@@ -1,1 +1,76 @@
 # Backend
+Great question! Understanding how a PATCH request works is essential for building RESTful applications.
+
+🧩 What is a PATCH Request?
+A PATCH request is an HTTP method used to partially update a resource on the server.
+
+Unlike PUT, which replaces the entire resource, PATCH only updates specific fields.
+
+Commonly used when editing a user profile, updating settings, or changing one or two fields of a database record.
+
+🧱 Example Flow of a PATCH Request in a Web App
+Let’s go through the flow with your app example (editing a user profile):
+
+🔁 1. Client (Browser) Sends Form via POST + Method Override
+HTML forms do not support PATCH or PUT directly.
+
+So we use this trick:
+
+html
+Copy code
+<form method="POST" action="/user/123?_method=PATCH">
+  <input name="username" value="newname">
+  <input name="email" value="newemail@example.com">
+  <button type="submit">Update</button>
+</form>
+The ?_method=PATCH part tells Express (with the help of method-override) to treat the POST as a PATCH.
+
+🧠 2. Express Uses Method Override Middleware
+You have this in your code:
+
+js
+Copy code
+const methodOverride = require("method-override");
+app.use(methodOverride("_method"));
+This allows Express to detect _method=PATCH in the query string and convert the POST into a PATCH request internally.
+
+📥 3. Express Handles the PATCH Route
+Your route looks like this:
+
+js
+Copy code
+app.patch("/user/:id", (req, res) => {
+    const { id } = req.params;
+    const { username, email } = req.body;
+
+    const updateQuery = "UPDATE profile SET username = ?, email = ? WHERE id = ?";
+    connection.query(updateQuery, [username, email, id], (err, result) => {
+        if (err) return res.send("Error updating user.");
+        res.redirect("/allusers");
+    });
+});
+Here's what happens:
+
+id comes from the URL (e.g., /user/123)
+
+username and email come from the form body
+
+The SQL query updates only those fields (not the whole record)
+
+⚡ Final Result:
+You visit /user/123/edit
+
+Submit the form
+
+The server processes the PATCH request
+
+Database gets updated (e.g., new username & email)
+
+You get redirected to the user list
+
+✅ Benefits of PATCH:
+Efficient: Only updates what's needed
+
+Standardized: Follows REST conventions
+
+Flexible: Allows partial updates
--- database/index.js
+++ database/index.js
@@ -1,8 +1,13 @@
 const mysql=require("mysql2");
-
-
-
-
+const { faker } = require('@faker-js/faker');
+const express=require("express");
+const app=express();
+let port=8080;
+const path=require("path");
+const methodOverride=require("method-override");
+const { constants } = require("buffer");
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({extended:true}));
 
 const connection=mysql.createConnection({         // to connect the database 
     host:"localhost",
@@ -11,25 +16,78 @@ const connection=mysql.createConnection({         // to connect the database
     password:"Mysql@4688",
 });
 
-let q="show tables";
 
-let insertquery="insert into user (id,age,name,email,followers,following) values ?";
+app.set("views engine","ejs");                      // this is to set template in home page and other pages
+app.set("views",path.join(__dirname,"/views"));
 
-let new_user=[
-    [1999,16,"krishna garg","kg@gmail.com",123,12],
-    [10001,112,"Dhruv","grd@gmail.com",155,156]
-];
 
-connection.query(insertquery,[new_user],(err,result)=>{
-    if(err){
-        console.log("The error occured in the insetion of the data",err.message);
-        connection.end();
-        return;
-    }
-    console.log("The insertion was successful thankyou!");
-    connection.end();
+
+// let create_tq="create table profile( id int,username varchar(200),email varchar(200));"
+
+// connection.query(create_tq,(err,result)=>{
+//     if(err){
+//         console.log("The error occured in the creation of the table ");
+//     }
+//     else{
+//         console.log("created succesfully");
+//     }
+//     connection.end();
+// });
+
+
+
+
+// let q="show tables";
+
+ let insertquery="insert into user (id,age,name,email,followers,following) values ?";
+
+// let new_user=[
+//     [1999,16,"krishna garg","kg@gmail.com",123,12],
+//     [10001,112,"Dhruv","grd@gmail.com",155,156]
+// ];
+
+// connection.query(insertquery,[new_user],(err,result)=>{
+//     if(err){
+//         console.log("The error occured in the insetion of the data",err.message);
+//         connection.end();
+//         return;
+//     }
+//     console.log("The insertion was successful thankyou!");
+//     connection.end();
     
-});
+// });
+
+
+
+// ----insert in bulk
+
+
+// function createRandomUser() {
+//   return [
+//     faker.string.uuid(),
+//      faker.internet.username(), // before version 9.1.0, use userName()
+//      faker.internet.email(),
+//   ];
+// }
+
+// let data=[];
+// for( i=0;i<100;i++){
+//     data.push(createRandomUser());
+//     }
+
+// let insertqueryprofil="insert into profile (id,username,email) values ?";
+// connection.query(insertqueryprofil,[data],(err,result)=>{
+//     if(err){
+//         console.log("error occured in the bulk insert",err.message);
+
+//     }
+//     else{
+//         console.log("inertion in bulk was successful");
+//     }
+//     connection.end();
+// });
+
+
 
 
 
@@ -58,3 +116,82 @@ connection.query(insertquery,[new_user],(err,result)=>{
     // connection.end();
 
     // });
+
+
+
+    app.get("/",(req,res)=>{                      // this is the home page route 
+        //res.send("welcome to home page");
+        let qry='select count(*) from profile';
+        try{
+            connection.query(qry,(err,result)=>{
+                if(err) throw err;
+                let count=result[0]["count(*)"];
+                res.render("home.ejs",{count});
+            });
+        }
+        catch(err){
+            res.send("some error occured");
+        }
+    });
+
+    app.get("/allusers",(req,res)=>{
+        let qry='select * from profile';
+        try{
+            connection.query(qry,(err,result)=>{
+                if(err) throw err;
+                let users=result;
+                res.render("allprofiles.ejs",{users});
+            });
+        }
+        catch(err){
+            res.send("some error occured");
+        }
+    })
+    app.get("/user/:id/edit",(req,res)=>{
+        let usrparam=req.params;
+        let editq=`select * from profile where id='${usrparam.id}'`;
+        try{
+            connection.query(editq,(err,result)=>{
+                if(err) throw err;
+                let user=result[0];
+                res.render("edit.ejs",{ user });
+                console.log(user);
+            });
+        }
+        catch(err){
+            res.send("some error occured in the edit section");
+        }
+    })
+
+
+    //UPDATE  DATABASE ROUTE 
+    app.patch("/edit/:id",(req,res)=>{
+        let {id}=req.params;
+        let {password: formpass,username:formusername}=req.body;
+        let q=`select * from profile where id='${id}'`;
+        try{
+            connection.query(q,(err,result)=>{
+                if(err) throw err;
+                let user=result[0];
+                if(formpass!==user.email){
+                    res.send("Wrong password");
+                }
+                else{
+                    let updateq=`update profile set username='${formusername}' where id='${id}'`;
+                    connection.query(updateq,(err,result)=>{
+                        if(err) throw err;
+                        res.redirect("/allusers")
+                    });
+                }
+                //res.send(user)
+        });
+        }
+        catch(err){
+            console.log(err);
+            res.send("An error occured in the change ");
+        }
+    });
+
+    app.listen(port,()=>{                               // this is command to start the engine server
+        console.log("Server is litening to port 8080");
+    })
